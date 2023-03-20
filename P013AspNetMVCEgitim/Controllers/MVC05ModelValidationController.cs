10957ca2d29f649959d13944d767da2af1027bf9
utkukemall <utkukemal1999@gmail.com>
2023-03-20 20:51:31 +0300

    20-03-2023 ders1

---
+++ P013AspNetMVCEgitim/Controllers/MVC05ModelValidationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P013AspNetMVCEgitim.Models;
+
+namespace P013AspNetMVCEgitim.Controllers
+{
+    public class MVC05ModelValidationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult UyeListesi()
+        {
+            return View();
+        }
+        public IActionResult YeniUye()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult YeniUye(Uye uye)
+        {
+            return View();
+        }
+    }
+}
---
+++ P013AspNetMVCEgitim/Models/Uye.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P013AspNetMVCEgitim.Models
+{
+    public class Uye 
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Ad Alanı Boş Geçilemez!")]
+        public string Ad { get; set; }
+        [Required(ErrorMessage = "{0} Alanı Boş Geçilemez!")]
+        public string Soyad { get; set; }
+        public string? Email { get; set; }
+        public string? Telefon { get; set; }
+        public string? TcKimlikNo { get; set; }
+        public DateTime? DogumTarihi { get; set; }
+        public string KullaniciAdi { get; set; }
+        public string Sifre { get; set; }
+        public string SifreTekrar { get; set; }
+
+        //String'in yanına soru işareti (?) eklediğimiz zaman zorunlu halden çıkıyor.
+    }
+}
