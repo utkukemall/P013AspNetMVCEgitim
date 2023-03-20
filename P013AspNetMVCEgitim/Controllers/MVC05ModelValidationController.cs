88d19f8f786e4eb6b028630786642de2f8ba5d24
utkukemall <utkukemal1999@gmail.com>
2023-03-20 22:55:03 +0300

    20.03.2023 Model Validation

--- P013AspNetMVCEgitim/Controllers/MVC05ModelValidationController.cs
+++ P013AspNetMVCEgitim/Controllers/MVC05ModelValidationController.cs
@@ -5,13 +5,15 @@ namespace P013AspNetMVCEgitim.Controllers
 {
     public class MVC05ModelValidationController : Controller
     {
+        UyeContext context = new UyeContext();
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult UyeListesi()
         {
-            return View();
+            var model = context.Uyes.ToList(); // Context içinde yer alan Uyes tablosundaki verileri listele ve model isimli değişkene aktar.
+            return View(model); // View sayfasına model bu şekilde gönderiliyor. 
         }
         public IActionResult YeniUye()
         {
@@ -20,11 +22,13 @@ namespace P013AspNetMVCEgitim.Controllers
         [HttpPost]
         public IActionResult YeniUye(Uye uye)
         {
-            if (ModelState.IsValid) // eğer parantez içerisinde gönderilen üye nesnesi validasyon kurallarına uygunsa 
+            if (ModelState.IsValid) // eğer parantez içerisinde gönderilen uye nesnesi validasyon kurallarına uygunsa 
             {
-                // Bu bloktaki kodları çalıştır. Mesela gönderilen üye nesnesini veritabanına ekle.
-                
-            }
+                // Bu bloktaki kodları çalıştır. Mesela gönderilen uye nesnesini veritabanına ekle.
+                context.Uyes.Add(uye); // View'dan gönderilen uye nesnesini context üzerindeki Uyes tablosuna ekle
+                context.SaveChanges(); // üst satırdaki ekleme işlemini kaydet.
+                return RedirectToAction("UyeListesi");
+            } 
             else
             {
                 ModelState.AddModelError("", "Lütfen Zorunlu Alanları Doldurunuz!"); 
--- P013AspNetMVCEgitim/Models/Uye.cs
+++ P013AspNetMVCEgitim/Models/Uye.cs
@@ -5,16 +5,25 @@ namespace P013AspNetMVCEgitim.Models
     public class Uye 
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "Ad Alanı Boş Geçilemez!")]
+        [Required(ErrorMessage = "Ad Alanı Boş Geçilemez!"), StringLength(50)] //StringLength ile ad alanına kaç karakter gönderebileceğini sınırlayabiliriz.
         public string Ad { get; set; }
-        [Required(ErrorMessage = "{0} Alanı Boş Geçilemez!")]
+        [Required(ErrorMessage = "{0} Alanı Boş Geçilemez!"), StringLength(50)]
         public string Soyad { get; set; }
+        [Required(ErrorMessage = "Geçersiz Mail Girdiniz!"), StringLength(50)]
         public string? Email { get; set; }
+        [Required(ErrorMessage = "Geçersiz Telefon Numarası Girdiniz!"), StringLength(50)]
         public string? Telefon { get; set; }
+        [Display(Name ="Tc Kimlik Numarası")] // Ekranda TcKimlikNo yerine Tc Kimlik Numarası yazsın.
+        [MinLength(11, ErrorMessage="{0} 11 Karakter Olmalıdır!")]
+        [MaxLength(11, ErrorMessage = "{0} 11 Karakter Olmalıdır!")]
         public string? TcKimlikNo { get; set; }
+        [Display(Name = "Doğum Tarihi")]
         public DateTime? DogumTarihi { get; set; }
+        [Display(Name = "Kullanıcı Adı")]
         public string? KullaniciAdi { get; set; }
+        [Display(Name = "Şifre")]
         public string? Sifre { get; set; }
+        [Display(Name = "Şifre Tekrar")]
         public string? SifreTekrar { get; set; }
 
         // String'in yanına soru işareti (?) eklediğimiz zaman zorunlu halden çıkıyor.
