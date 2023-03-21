d3f32048a604c48463d499e49b7f9b2cb805701a
utkukemall <utkukemal1999@gmail.com>
2023-03-21 20:50:26 +0300

    21.03.2023 Ders v1

--- P013AspNetMVCEgitim/Controllers/MVC05ModelValidationController.cs
+++ P013AspNetMVCEgitim/Controllers/MVC05ModelValidationController.cs
@@ -15,6 +15,8 @@ namespace P013AspNetMVCEgitim.Controllers
             var model = context.Uyes.ToList(); // Context içinde yer alan Uyes tablosundaki verileri listele ve model isimli değişkene aktar.
             return View(model); // View sayfasına model bu şekilde gönderiliyor. 
         }
+
+
         public IActionResult YeniUye()
         {
             return View();
@@ -35,5 +37,50 @@ namespace P013AspNetMVCEgitim.Controllers
             }
             return View();
         }
+
+
+        public IActionResult UyeDuzenle(int id)
+        {
+            var kayit = context.Uyes.Find(id); // adres çubuğundaki route üzerinden gönderilen id ile eşleşen kaydı bul.
+            return View(kayit);
+        }
+        [HttpPost]
+        public IActionResult UyeDuzenle(Uye uye)
+        {
+            if (ModelState.IsValid) // eğer parantez içerisinde gönderilen uye nesnesi validasyon kurallarına uygunsa 
+            {
+                context.Uyes.Update(uye); // View'dan gönderilen uye nesnesini güncelle
+                context.SaveChanges(); // üst satırdaki üye düzenleme işlemini kaydet.
+                return RedirectToAction("UyeListesi");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Lütfen Zorunlu Alanları Doldurunuz!");
+            }
+            return View();
+        }
+
+
+        public IActionResult UyeSil(int id)
+        {
+            var kayit = context.Uyes.Find(id); // adres çubuğundaki route üzerinden gönderilen id ile eşleşen kaydı bul.
+            return View(kayit);
+        }
+        [HttpPost]
+        public IActionResult UyeSil(Uye uye)
+        {
+            try
+            {
+                context.Uyes.Remove(uye);
+                context.SaveChanges(); // üst satırdaki silme işlemini kaydet.
+                return RedirectToAction("UyeListesi");
+            }
+            catch (Exception hata)
+            {
+                ModelState.AddModelError("", "Hata Oluştu!" + hata.Message);
+            }
+            return View();
+        }
+
     }
 }
---
+++ P013AspNetMVCEgitim/Controllers/MVC06SectionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P013AspNetMVCEgitim.Controllers
+{
+    public class MVC06SectionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
