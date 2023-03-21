0012d9a9444b4fc6f1ba804be4ba97b5f70baa8b
utkukemall <utkukemal1999@gmail.com>
2023-03-21 23:08:46 +0300

    21.03.2023 Dersi

---
+++ P013AspNetMVCEgitim/Controllers/MVC07PartialController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P013AspNetMVCEgitim.Models;
+
+namespace P013AspNetMVCEgitim.Controllers
+{
+    public class MVC07PartialController : Controller
+    {
+        public IActionResult Index()
+        {
+            Kullanici kullanici = new Kullanici();
+            kullanici.Ad = "Utku";
+            kullanici.Soyad = "Çiçek";
+            return View(kullanici);
+        }
+    }
+}
---
+++ P013AspNetMVCEgitim/Controllers/MVC08ViewResultsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P013AspNetMVCEgitim.Controllers
+{
+    public class MVC08ViewResultsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Yonlendir(string arananDeger)
+        {
+            //return Redirect("/Home"); // Bu action tetiklendiğinde uygulama anasayfaya gitsin.
+            //return Redirect($"/Home?kelime={arananDeger}"); // Burada soru işaretinden (?) sonraki kısım querystring yöntemiyle adres çubuğu üzerinden veri yollamak için kullanılır.
+            return Redirect("https://getbootstrap.com/"); // Bu şekilde harici bir siteye de yönlendirme yapabiliyoruz.
+        }
+
+        public IActionResult ActionaYonlendir()
+        {
+            //return RedirectToAction("Index"); // metot çalıştığında aynı controllerdaki bir actiona yönlendirmemizi sağlar.
+            return RedirectToAction("Index","Home"); // metot çalıştığında farklı bir controller dakii actiona bu şekilde yönlendirebiliriz.
+        }
+
+        public IActionResult RouteYonlendir()
+        {
+            return RedirectToRoute("Default", new { controller="Home", action="Index", id=18}); // Metot çalıştığı zaman route sistemiyle yönlendirme yapmamızı sağlar.
+        }
+    }
+}
